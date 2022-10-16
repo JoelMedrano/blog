7477a411d736df3f2ba0d3a1e009c07eaeacbe00
Joel <jvmedranog@gmail.com>
2022-10-16 15:57:36 -0500

    #15 Auth JWT Proteccion de rutas con middlewarwe

--- routes/api.php
+++ routes/api.php
@@ -39,8 +39,20 @@ use App\Http\Controllers\ArticleController;
 //     return 204;
 // });
 
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
+    Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
+    Route::post('articles', 'App\Http\Controllers\ArticleController@store');
+    Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
+    Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+});
+
+/* Route::get('articles', 'App\Http\Controllers\ArticleController@index');
 Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
 Route::post('articles', 'App\Http\Controllers\ArticleController@store');
 Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
-Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete'); */
