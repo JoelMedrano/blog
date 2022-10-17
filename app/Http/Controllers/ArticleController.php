3b6a25a595ebd00205b6b5ae22c8998c398862b6
Joel <jvmedranog@gmail.com>
2022-10-17 00:20:41 -0500

    #29 proceso de crear y descargar imagen

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ArticleController extends Controller
 {
@@ -24,6 +25,12 @@ class ArticleController extends Controller
     {
         return response()->json(new ArticleResource($article),200);
     }
+
+    public function image(Article $article)
+    {
+        return response()->download(public_path(Storage::url($article->image)), $article->title);
+    }
+
     public function store(Request $request)
     {
 
@@ -31,9 +38,15 @@ class ArticleController extends Controller
             'title' => 'required|string|unique:articles|max:255',
             'body' => 'required',
             'category_id' => 'required|exists:categories,id',
+            'image' => 'required|image|dimensions:min_width=200,min_height=200',
         ], self::$messages);
 
-        $article = Article::create($request->all());
+        $article = new Article($request->all());
+        $path = $request->image->store('public/articles');
+
+        $article->image = 'articles/' . basename($path);
+        $article->save();
+
         return response()->json(new ArticleResource($article), 201);
 
     }
